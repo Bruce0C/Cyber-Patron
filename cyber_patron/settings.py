81422b56f00af8aff7ae30b1343f517870d6564e
Bruce0C <pvlm5xi2@students.codeinstitute.net>
2025-11-18 19:17:01 +0000

    Creat checkout app and models

--- cyber_patron/settings.py
+++ cyber_patron/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     # apps
     'home',
     'products',
-    'bag'
+    'bag',
+    'checkout'
 ]
 
 MIDDLEWARE = [
@@ -121,16 +122,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'NumericPasswordValidator',
     },
 ]
 
