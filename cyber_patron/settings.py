bb76597e0ae9898ffc03c0f55da299585c7706da
Bruce0C <pvlm5xi2@students.codeinstitute.net>
2025-11-19 11:29:04 +0000

    Add deployement setting

--- cyber_patron/settings.py
+++ cyber_patron/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import env
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,6 +31,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = [
     '127.0.0.1',  # local host
     'localhost',  # listen for stripe webhooks
+    'https://cyber-patron-34aa969be4ad.herokuapp.com/'  # heroku application
 ]
 
 
@@ -126,14 +129,18 @@ WSGI_APPLICATION = 'cyber_patron.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        #   'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#  'default': {
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#        #   'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 
+# Only use once databasese are working
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
