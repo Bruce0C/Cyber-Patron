d63f56334d3f84aa9c0179b84f376f836a844911
Bruce0C <pvlm5xi2@students.codeinstitute.net>
2026-03-12 02:35:42 +0000

    Remove production static and media URL overrides from settings

--- cyber_patron/settings.py
+++ cyber_patron/settings.py
@@ -205,10 +205,6 @@ STATICFILES_LOCATION = 'static'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 MEDIAFILES_LOCATION = 'media'
 
-# Override static and media URLs in production
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
 
 FREE_DELIVERY_THRESHOLD = 0  # not required
 STANDARD_DELIVERY_PERCENTAGE = 0  # not required
